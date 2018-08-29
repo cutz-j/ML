943168a7732e7896c15d491c14b5d103e3d1b701
cutz-j <cutz309@gmail.com>
2018-08-29 22:00:52 +0900

    Add lec3

--- ML for everyone lec/lec3[cost].py
+++ ML for everyone lec/lec3[cost].py
@@ -1,20 +1,41 @@
-import tesnorflow as tf
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-X=[1,2,3]
-Y=[1,2,3]
+x=[1,2,3]
+y=[1,2,3]
 
-W = tf.placeholder(tf.float32)
+X=tf.placeholder(tf.float32)
+Y=tf.placeholder(tf.float32)
+
+#W = tf.placeholder(tf.float32)
+W= tf.Variable(tf.random_normal([1]), name='weight')
 hypothesis=W*X
 
-cost=tf.reduce.mean(tf.square(hypothesis-W))
+cost=tf.reduce_mean(tf.square(hypothesis-Y))
+
+
+
+#W_val=[]
+#cost_val=[]
+#for i in range(-30, 50):
+#    feed_W=i*0.1
+#    curr_cost, curr_W=sess.run([cost, W], feed_dict={W: feed_W})
+#    W_val.append(curr_W)
+#    cost_val.append(curr_cost)
+#    
+
+learning_rate=0.1
+gradient=tf.reduce_mean((W*X-Y)*X)
+descent=W-learning_rate * gradient
+update=W.assign(descent)
 
 sess=tf.Session()
-
 sess.run(tf.global_variables_initializer())
 
-W_val=[]
-cost_val=[]
-for i in range(-30, 50):
-    feed_W=i*0.1
-    cuur_cost, curr_W=sess.run[]
+for step in range(21):
+    sess.run(update, feed_dict={X: x, Y: y})
+    print(step, sess.run(cost, feed_dict={X: x, Y: y}), sess.run(W))
+
+
+#plt.plot(W_val, cost_val)
+#plt.show()
