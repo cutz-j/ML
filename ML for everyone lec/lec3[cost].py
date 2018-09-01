3d6042d91397939d2caacf436aec767e7eb9a762
cutz-j <cutz309@gmail.com>
2018-09-01 11:40:19 +0900

    Update lec3

--- ML for everyone lec/lec3[cost].py
+++ ML for everyone lec/lec3[cost].py
@@ -39,3 +39,48 @@ for step in range(21):
 
 #plt.plot(W_val, cost_val)
 #plt.show()
+
+X=[1,2,3]
+Y=[1,2,3]
+
+W = tf.Variable(-3.0)
+
+hypothesis = X*W
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train=optimizer.minimize(cost)
+
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(100):
+    print(step, sess.run(W))
+    sess.run(train)
+
+
+# 비
+X=[1,2,3]
+Y=[1,2,3]
+
+W=tf.Variable(5.)
+hypothesis=X*W
+gradient=tf.reduce_mean((W*X-Y)*X)*2
+
+cost=tf.reduce_mean(tf.square(hypothesis-Y))
+
+optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+# Get gradients
+gvs=optimizer.compute_gradients(cost, [W])
+# Apply grad
+apply_gradients = optimizer.apply_gradients(gvs)
+
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(100):
+    print(step, sess.run([gradient, W, gvs]))
+    sess.run(apply_gradients)
+
