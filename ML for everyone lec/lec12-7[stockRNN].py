cb0a2bbdf27dfa99b24b10da552fc933d2a2e394
cutz-j <cutz309@gmail.com>
2018-11-05 22:10:33 +0900

    Update ML for everyone lec/lec12-7[stockRNN].py

--- ML for everyone lec/lec12-7[stockRNN].py
+++ ML for everyone lec/lec12-7[stockRNN].py
@@ -1,5 +1,4 @@
 ### stock daily RNN-LSTM ###
-
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -11,21 +10,28 @@ tf.set_random_seed(777)
 tf.reset_default_graph()
 
 ## parameter ##
-seq_length = 15
+seq_length = 7
 data_dim = 5
 output_dim = 1
-hidden_size = 10
-learning_rate = 0.01
-iteration = 2000
+hidden_size = 30
+learning_rate = 0.03
+iteration = 5000
 
 ### 데이터 전처리 ###
 stock_data = np.loadtxt("d:/data/prac/stock_daily.csv", delimiter=',', skiprows=1)
 stock_data = stock_data[::-1] # shape (732, 5)
 
+# train scaling #
+mm1 = MinMaxScaler()
+stock_data_x = mm1.fit_transform(stock_data[:, :-1])
+stock_data_y = mm1.fit_transform(stock_data[:, [-1]])
+stock_data = np.concatenate((stock_data_x, stock_data_y), axis=1)
+
 ## split ## --> 시계열(시간순)
 train_size = int(len(stock_data) * 0.7)
 train_set = stock_data[:train_size, :] # shape(512, 5)
 test_set = stock_data[train_size:, :] # test(220, 5)
+
 
 # RNN data building #
 def build(time_series, seq_length):
@@ -38,16 +44,67 @@ def build(time_series, seq_length):
         y_data.append(y_tmp)
     return np.array(x_data), np.array(y_data)
 
+x_train, y_train = build(train_set, seq_length)
+x_test, y_test = build(test_set, seq_length)
+
+## RNN building ##
+# cell #
+def lstm_cell():
+    cell = rnn.BasicLSTMCell(hidden_size, state_is_tuple=True, activation=tf.tanh)
+    return cell
+
+#cell = rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True, activation=tf.tanh)
+cell = rnn.MultiRNNCell([lstm_cell() for _ in range(2)], state_is_tuple=True)
+#
+X = tf.placeholder(dtype=tf.float32, shape=[None, seq_length, data_dim])
+y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+#
+## 초기화 #
+output, _state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32) 
+Y_pred = tf.contrib.layers.fully_connected(output[:, -1], output_dim, activation_fn=None) # last cell output --> 15일 뒤
 
 
+# cost #
+cost = tf.reduce_sum(tf.square(Y_pred - y)) # sum of sq --> 수치 예측이기 때문에 sq loss가 필요 없다.
+opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train = opt.minimize(cost)
 
+# MSE # --> mean squared error
+targets= tf.placeholder(tf.float32, [None, 1])
+predicts = tf.placeholder(tf.float32, [None, 1])
+MSE = tf.sqrt(tf.reduce_mean(tf.square(predicts - targets)))
 
+## session ##
+# training#
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+for i in range(iteration):
+    cost_val, _, out= sess.run([cost, train, output], feed_dict={X: x_train, y: y_train})
+    if i % 300 == 0:
+        print(cost_val)
 
+# predict #
+y_hat_train = sess.run(Y_pred, feed_dict={X: x_train})
+y_hat = sess.run(Y_pred, feed_dict={X: x_test})
+RMSE_train = sess.run(MSE, feed_dict={targets: y_train, predicts: y_hat_train})
+RMSE = sess.run(MSE, feed_dict={targets: y_test, predicts: y_hat})
+print("RMSE_train: ", RMSE_train)
+print("RMSE: ", RMSE)
 
+sess.close()
 
+y_hat = mm1.inverse_transform(y_hat)
+y_test = mm1.inverse_transform(y_test)
 
+plt.figure()
+plt.plot(y_train, 'r-')
+plt.plot(y_hat_train, 'b-')
+plt.show()
 
-
+plt.figure()
+plt.plot(y_test, 'r-')
+plt.plot(y_hat, 'b-')
+plt.show()
 
 
 
