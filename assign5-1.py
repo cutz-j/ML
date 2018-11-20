de4b8c57cf5ea18540d063fc70e8f5170f80d36a
cutz-j <cutz309@gmail.com>
2018-11-20 09:28:58 +0900

    Update assign5-1.py

--- assign5-1.py
+++ assign5-1.py
@@ -12,7 +12,7 @@ X = all_data['X'] # shape(12, 1)
 X_test = all_data['Xtest'] # (21, 1)
 X_cv = all_data['Xval'] # (21, 1)
 y = all_data['y'] # (12, 1)
-ytest = all_data['ytest'] # (21, 1)
+y_test = all_data['ytest'] # (21, 1)
 y_cv = all_data['yval'] # (21, 1)
 
 ## 1.1 Visualizing the dataset ##
@@ -34,10 +34,10 @@ def cost(X, W, y, myld):
     m = len(X)
     return (1 / 2) * np.mean(np.square((hypothesis(X, W) - y))) + (myld * (1 / (2 * m)) * (np.sum(np.square(W[1:]))))
      
-#print(cost(X, init_w, y, 1.)) # 303.993
+print(cost(X, init_w, y, 1.)) # 303.993
 
 ## 1.3 Regularized linear regression gradient ##
-def gradientDescent(X, W, y, myld, learning_rate=0.001, iteration=1000):
+def gradientDescent(X, W, y, myld, learning_rate=0.006, iteration=2000):
     m = len(X)
     for i in range(iteration):
         grad = (1. / m) * np.sum((hypothesis(X, W) - y) * X, axis=0)
@@ -51,11 +51,11 @@ def gradientDescent(X, W, y, myld, learning_rate=0.001, iteration=1000):
 new_W = gradientDescent(X, init_w, y, 0, 1.49e-1, 1000) # [-15.30, 598.25]
 
 ## 1.4 Fitting linear regression
-#plt.figure()
-#plt.plot(X[:,1], y, "rx")
-#plt.plot(X[:,1], hypothesis(X, new_W), "b-")
-#plt.show()
-#    
+plt.figure()
+plt.plot(X[:,1], y, "rx")
+plt.plot(X[:,1], hypothesis(X, new_W), "b-")
+plt.show()
+    
     
 ### 2 Bias-variance ###   
 ## 2.1 Learning curves ##
@@ -66,15 +66,15 @@ def learning_curve(X, X_cv, y, y_cv, W, myld):
         train_set = X[:i, :]
         y_set = y[:i, :]
         m_list.append(len(train_set))
-        new_w = gradientDescent(train_set, W, y_set, myld=0)
-        x_cost.append(cost(train_set, new_w, y_set, myld=0))
-        cv_cost.append(cost(X_cv, new_w, y_cv, myld=0))
+        new_w = gradientDescent(train_set, W, y_set, myld)
+        x_cost.append(cost(train_set, new_w, y_set, myld))
+        cv_cost.append(cost(X_cv, new_w, y_cv, myld))
     plt.figure()
     plt.plot(m_list, x_cost, 'r-')
     plt.plot(m_list, cv_cost, 'b-')
     plt.show()
     
-#learning_curve(X, X_cv, y, y_cv, init_w, myld=0)
+learning_curve(X, X_cv, y, y_cv, init_w, myld=0)
 
 ### 3 polynomial regression ###
 def poly(X, p):
@@ -90,27 +90,29 @@ X_poly = poly(X, 5)
 def normalize(X):
     Xnorm = X.copy()
     stored_feature_means = np.mean(Xnorm, axis=0) #column-by-column
-    Xnorm[:,1:] = Xnorm[:,1:] - stored_feature_means[1:]
+    stored_feature_means = stored_feature_means.reshape(1, len(stored_feature_means))
+    Xnorm[:,1:] = Xnorm[:,1:] - stored_feature_means[:, 1:]
     stored_feature_stds = np.std(Xnorm, axis=0, ddof=1)
-    Xnorm[:,1:] = Xnorm[:,1:] / stored_feature_stds[1:]
+    stored_feature_stds = stored_feature_stds.reshape(1, len(stored_feature_stds))
+    Xnorm[:,1:] = Xnorm[:,1:] / stored_feature_stds[:, 1:]
     return Xnorm, stored_feature_means, stored_feature_stds
 
 X_norm, means, stds = normalize(X_poly) # shape(12, 9)
 init_w = np.ones(shape=(X_norm.shape[1], 1))
 new_w = gradientDescent(X_norm, init_w, y, myld=0)
     
-#def plotFit(W, means, stds):
-#    n_points_to_plot = 50
-#    xvals = np.linspace(-55, 55, n_points_to_plot)
-#    xmat = np.ones((n_points_to_plot, 1))
-#    xmat = np.insert(xmat, xmat.shape[1], xvals.T, axis=1)
-#    xmat = poly(xmat, len(W)-2)
-#    xmat[:,1:] = xmat[:,1:] - means[1:]
-#    xmat[:,1:] = xmat[:,1:] / stds[1:]
-#    plot()
-#    plt.plot(xvals, hypothesis(xmat, W),'b--')
-#
-#plotFit(new_w, means, stds)
+def plotFit(W, means, stds):
+    n_points_to_plot = 50
+    xvals = np.linspace(-55, 55, n_points_to_plot)
+    xmat = np.ones((n_points_to_plot, 1))
+    xmat = np.insert(xmat, xmat.shape[1], xvals.T, axis=1)
+    xmat = poly(xmat, len(W)-1)
+    xmat[:,1:] = xmat[:,1:] - means[:, 1:]
+    xmat[:,1:] = xmat[:,1:] / stds[:, 1:]
+    plot()
+    plt.plot(xvals, hypothesis(xmat, W),'b--')
+
+plotFit(new_w, means, stds)
 
 def poly_learning_curve(X, X_cv, y, y_cv, W, myld):
     m = len(X)
@@ -127,8 +129,10 @@ def poly_learning_curve(X, X_cv, y, y_cv, W, myld):
     plt.plot(m_list, cv_cost, 'b-')
     plt.show()
 
+new_w = gradientDescent(X_norm, init_w, y, myld=100)
+plotFit(new_w, means, stds)
 X_cv_norm, means_cv, stds_cv = normalize(poly(X_cv, 5))
-poly_learning_curve(X_norm, X_cv_norm, y, y_cv, init_w, myld=1)
+poly_learning_curve(X_norm, X_cv_norm, y, y_cv, init_w, myld=0)
 
 lambdas = np.linspace(0,5,20)
 err_train, err_val = [], []
@@ -145,3 +149,7 @@ plt.figure()
 plt.plot(lambdas, err_train, 'r-')
 plt.plot(lambdas, err_val, 'b-')
 plt.show()
+
+new_w = gradientDescent(X_norm, init_w, y, myld=2)
+x_test_norm, means, stds = normalize(poly(X_test, 5))
+print(cost(x_test_norm, new_w, y_test, myld=0))
